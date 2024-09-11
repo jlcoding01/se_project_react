bb878a612a7754e2e67eb1d2c0d8b0b0e2587397
Jerry Liu <ljliu89@gmail.com>
2024-09-11 15:30:00 -0700

    modify baseUrl

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,9 @@
 import { request } from "./api";
 
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwr.showmyhomes.com"
+    : "http://localhost:3001";
 
 // function checkResponse(res) {
 //   return res.ok ? res.json() : Promise.reject(`Error:${res.status}`);
